d4ac7227ae4c96f5fcd5de8e786a9932024c8ac1
sir-game-a-lot <jorjdboss@gmail.com>
2014-02-18 01:28:58 +0530

    Facebook Android integrated
    TODO: post score, invite freinds, submit scores

---
+++ Assets/Scripts/FacebookManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FacebookManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Awake ()
+	{
+		FB.Init(faceBookInitialized);
+	}
+	
+	void faceBookInitialized()
+	{
+		if( !FB.IsLoggedIn )
+			FB.Login("email,publish_actions",loggedIn);
+		else
+			ShowDialog();
+	}
+
+	void loggedIn(FBResult fbResult)
+	{
+		ShowDialog();
+	}
+
+	void ShowDialog()
+	{
+		FB.Feed(linkCaption: "I just swooshed through 28 usb cables in Flappy nerd!!!",
+		        picture:"http://www.taste.com.au/images/articles/untitled-102121315.jpg"
+		        );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
